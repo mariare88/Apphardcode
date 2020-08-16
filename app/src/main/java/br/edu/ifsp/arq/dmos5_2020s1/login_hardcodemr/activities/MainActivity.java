79709b07f49f146fd9bf758d2605e70334538d90
mariare88 <mariareginare88@gmail.com>
2020-08-16 11:18:07 -0300

    Versao 1 :implementada

--- app/src/main/java/br/edu/ifsp/arq/dmos5_2020s1/login_hardcodemr/activities/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/arq/dmos5_2020s1/login_hardcodemr/activities/MainActivity.java
@@ -7,25 +7,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import br.edu.ifsp.arq.dmos5_2020s1.login_hardcodemr.R;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    private EditText n1EditText;
-    private EditText n2EditText;
-    private Button validarButton;
+import static android.widget.Toast.LENGTH_SHORT;
+import static android.widget.Toast.makeText;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private EditText usuarioEditText;
+    private EditText senhaEditText;
+    private Button validarButton;
+    private TextView saidaTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        n1EditText = findViewById(R.id.edittext_n1);
-        n2EditText = findViewById(R.id.edittext_n2);
+        usuarioEditText = findViewById(R.id.edittext_usuario);
+        senhaEditText = findViewById(R.id.edittext_senha);
         validarButton = findViewById(R.id.button_ok);
-
+        saidaTextView = findViewById(R.id.textview_saida);
         validarButton.setOnClickListener(this);
 
     }
@@ -36,20 +40,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
     private void validar(){
-        double n1,n2,ok;
+        double usuario,senha,validar;
         try {
-            n1 = Double.valueOf(n1EditText.getText().toString());
-            n2 = Double.valueOf(n2EditText.getText().toString());
+            usuario = Double.valueOf(usuarioEditText.getText().toString());
+            senha = Double.valueOf(senhaEditText.getText().toString());
         }catch (NumberFormatException ex) {
-            n1 = 1710346;
-            n2 = 6430171;
-            Toast.makeText(context:this, text:"Erro no login", Toast.LENGTH_SHORT).show();
+            usuario = 0;
+            senha = 0;
 
+            Toast.makeText(this, "Erro no login",Toast.LENGTH_SHORT).show();
             return;
         }
-
-        Toast.makeText(context: this,String.format("Login ok"), Toast.LENGTH_SHORT).show();
-
-        }
+            Toast.makeText(this, "Erro no login",Toast.LENGTH_SHORT).show();
     }
+            //saidaTextView.setText(String.format("Bem vindo 1710346"));
+}
+
 
